7989774aefaf1cb76da15bba3954aec52e4e14bd
Alexandre Du lorier <alexandre.dulorier@epitech.eu>
2018-08-27 08:08:24 +0200

    osef

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ app.engine('html', ngExpressEngine({
 }));
 
 app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
+app.set('views', join(DIST_FOLDER, 'echo-universe/main'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -58,7 +58,7 @@ app.use(bodyParser.json());
 */
 app.use('/api', api);
 // Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
+app.get('*.*', express.static(join(DIST_FOLDER, 'echo-universe/main'), {
   maxAge: '1y'
 }));
 
